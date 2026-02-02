5ea500a239a7a3e659a1020afb80ff487fbf1bb6
Nijin Vinodan <nvinodan@presidio.com>
2026-02-02 20:34:50 +0530

    Added markdown

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const Message = ({ role, content }) => {
     return (
         <div className={`message ${role}`}>
             <div className="message-bubble">
-                {content.split('\n').map((line, i) => (
-                    <p key={i}>{line}</p>
-                ))}
+                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                    {content}
+                </ReactMarkdown>
             </div>
         </div>
     );
